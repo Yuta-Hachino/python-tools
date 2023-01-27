bcf1f586c5a07460a63b9186ee3604c67aaba795
hachino <program@eight8.tech>
2023-01-28 05:45:10 +0900

    変更があった場合のみAddする

--- search_and_git_add.py
+++ search_and_git_add.py
@@ -1,3 +1,6 @@
+# 任意で指定されたディレクトリ内を検索し、一致したファイル群にgit addを実行する。
+# ※対象ファイルが存在し且つ変更がある場合
+
 import sys
 import glob
 import git
@@ -11,6 +14,8 @@ files = glob.glob(searchTargetPath + searchWord, recursive=True)
 
 repo = git.Repo(searchTargetPath)
 
-for name in files:
-    print(name)
-    repo.git.add(name)
+# 変更があった場合のみAddする
+if repo.git.diff() == '':
+    for name in files:
+        print(name)
+        repo.git.add(name)
